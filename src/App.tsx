8bf60e309e72e42033fc3f677f0ac69961339fcf
Ester Michalkova <estimichalkova@gmail.com>
2025-12-23 17:57:11 +0100

    Set BrowserRouter basename for routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/kveta-transformation">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/o-mne" element={<About />} />
